e49f1e1dd6f842b0bef252bfa94c77b9a81933c0
Vishal <v.patel1093@gmail.com>
2017-12-18 13:29:01 -0500

    create controllers for index and show pages of events, configure routes

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,14 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @people = @event.people
+    respond_to do |format|
+        format.html
+        format.json
+    end
+  end
+end
